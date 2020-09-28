f97695e640ea993a45917cf64655763bef0a9e99
Kyle Eriss <kyle.eriss@gmail.com>
2020-09-28 18:32:19 -0400

    revisions

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ function getDogImage() {
       .then(response => response.json())
       .then(responseJson => 
         displayResults(responseJson))
-      //.catch(error => alert('Something went wrong. Try again later.'));
+      .catch(error => alert('Something went wrong. Try again later.'));
   }
   
   function displayResults(responseJson) {
     console.log(responseJson);
     let imageText = `<li><img src="${responseJson.message}" class="results-img"></li>`;
-    //replace the existing image with the new one
-    console.log(imageText);
     $('.results-img').append(imageText)
-    //display the results section
     $('.results').removeClass('hidden');
   }
   
@@ -21,11 +18,9 @@ function getDogImage() {
       event.preventDefault();
       $('.results-img').empty();
       let imageNumber = document.getElementById("selectNumber").value;
-      console.log(imageNumber);
       for (let i = 0; i < imageNumber; i++) {
           getDogImage();
       }
-      
     });
   }
   
