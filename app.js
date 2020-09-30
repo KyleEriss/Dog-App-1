a3fc6d3f08944b705e0fffdcf0435aa5a3579921
Kyle Eriss <kyle.eriss@gmail.com>
2020-09-30 03:40:26 -0400

    final revision

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(responseJson => displayResults(responseJson))
-  .catch(error => alert('Something went wrong. Try again later.'));
+'use strict'
+
+function getDogPics() {
+  let numOfDogs = $('#selectNumber').val();
+  fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
+    .then(response => response.json())
+    .then(responseJson => displayDogs(responseJson))
+    .catch(error => alert("Something went wrong, please try again"));
 }
-  
-  function displayResults(responseJson) {
-    console.log(responseJson);
-    let imageText = `<li><img src="${responseJson.message}" class="results-img"></li>`;
-    $('.results-img').append(imageText)
-    $('.results').removeClass('hidden');
+
+function consoleResponse() {
+  let numOfDogs = $('#selectNumber').val();
+  fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson.message))
+    .catch(error => alert("Something went wrong, please try again"));
+}
+
+function displayDogs(responseJson) {
+  for (let i = 0; i < responseJson.message.length; i++) {
+    $(".results").append(`<img src="${responseJson.message[i]}" class="results-img">`)
   }
-  
-  function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-      $('.results-img').empty();
-      let numbersError = `Please select a number between 1 and 50`;
-      let imageNumber = document.getElementById("selectNumber").value;
-      if (imageNumber < 1 || imageNumber > 50) {
-        return $(".results-img").html(numbersError);
-      }
-      for (let i = 0; i < imageNumber; i++) {
-          getDogImage();
-      }
-    });
-  }
-  
-  $(function() {
-    console.log('App loaded! Waiting for submit!');
-    watchForm();
+    $(".results").removeClass("hidden");
+}
+
+function clearImages() {
+  $(".results-img").remove();
+}
+
+
+function handleForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    clearImages();
+    let numOfDogs = $('#selectNumber').val();
+    let errorMessage = `Please select a number between 1 and 50`;
+    console.log(numOfDogs);
+    if (numOfDogs < 1 || numOfDogs > 50) {
+      return alert(errorMessage);
+    }
+    getDogPics();
+    consoleResponse();
   });
+}
+
+function dogImageApp() {
+  console.log("App is loaded, awaiting submission!");
+  handleForm();
+}
+
+$(dogImageApp);
